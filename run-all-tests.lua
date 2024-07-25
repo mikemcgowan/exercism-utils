1844fbd57d97be701928c6d5d793e72abaa43f0a
Mike McGowan <mike.mcgowan@getbridge.com>
2024-07-25 14:18:37 +0100

    Update

--- run-all-tests.lua
+++ run-all-tests.lua
@@ -8,21 +8,25 @@ if not file then
   print("Could not open exercises file " .. filename)
   os.exit(1)
 end
-
 local exercises = {}
 for line in file:lines() do
   table.insert(exercises, line)
 end
+file:close()
 
+local successful = 0
+local total = 0
 for _, exercise in pairs(exercises) do
   local attr = lfs.attributes(exercise)
   if attr and attr.mode == "directory" then
     lfs.chdir(exercise)
-    os.execute("exercism test")
+    local status, _, _ = os.execute("exercism test")
+    if status then successful = successful + 1 end
+    total = total + 1
     lfs.chdir("..")
   else
     print("Skipping '" .. exercise .. "' as it doesn't appear to be downloaded")
   end
 end
 
-file:close()
+print(successful .. " out of " .. total .. " succeeded")
